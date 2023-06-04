aab804764b22557f001fcacd8b18809b9ae5b399
danie1souzasilva <119264364+danie1souzasilva@users.noreply.github.com>
2023-06-04 16:29:54 -0300

    desafio UML

---
+++ .vscode/UMLdesafio/src/IphoneSete.java
@@ -0,0 +1,41 @@
+public class IphoneSete implements NavegadorInternet, AparelhoTelefonico{
+    public static void main(String[] args) {
+        
+    }
+
+    @Override
+    public void ligar() {
+        // TODO Auto-generated method stub
+        System.out.println("ligar");
+    }
+
+    @Override
+    public void atender() {
+        // TODO Auto-generated method stub
+        System.out.println("atender telefone");
+    }
+
+    @Override
+    public void correioDeVoz() {
+        // TODO Auto-generated method stub
+        System.out.println("menssagem");
+    }
+
+    @Override
+    public void exibirPagina() {
+        // TODO Auto-generated method stub
+        System.out.println("exibe pagina");
+    }
+
+    @Override
+    public void abrirNovaAba() {
+        // TODO Auto-generated method stub
+        System.out.println("abre nova aba");
+    }
+
+    @Override
+    public void atualizarPagina() {
+        // TODO Auto-generated method stub
+        System.out.println("atualiza pagina");
+    }
+}
---
+++ .vscode/UMLdesafio/src/NavegadorInternet.java
@@ -0,0 +1,5 @@
+public interface NavegadorInternet {
+    public void exibirPagina();
+	public void abrirNovaAba();
+	public void atualizarPagina();
+}
